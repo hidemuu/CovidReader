edcadda511a0b9b47cf370d475d4892b9d910c84
s-hidemitsu-miura <75873299+p-hidemitsu-miura@users.noreply.github.com>
2021-08-31 18:15:21 +0900

    20210831
    構造変更

---
+++ apps/csharp/Models/CovidReader.Repository/Urls.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CovidReader.Repository
+{
+    public static class Urls
+    {
+        public static readonly string RootPath = PathHelper.GetCurrentRootPath(Constants.RootName);
+
+        public static readonly string SqlLocalConnectionString = RootPath + @"assets\api\db.sqlite";
+
+        public static readonly string SqlCovidConnectionString = RootPath + @"assets\covid\covid.sqlite";
+
+        public static readonly string ServerPath = @"C:\xampp\htdocs\covid_reader\csharp\";
+
+        public static readonly string ServerUrl = @"http://localhost:81/covid_reader/csharp/";
+
+        /// <summary>
+        /// Base URL for the app's API service. A live test service is provided for convenience; 
+        /// however, you cannot modify any data on the server or deploy your own updates. 
+        /// To see the full functionality, deploy Contoso.Service using your own Azure account.
+        /// </summary>
+        public const string ApiUrl = @"http://customers-orders-api-prod.azurewebsites.net/api/";
+
+        /// <summary>
+        /// The Azure Active Directory (AAD) client id.
+        /// </summary>
+        public const string AccountClientId = "<TODO: Insert Azure client Id>";
+
+        /// <summary>
+        /// Connection string for a server-side SQL database.
+        /// </summary>
+        public const string SqlAzureConnectionString = "<TODO: Insert connection string>";
+
+        
+    }
+}
--- scripts/table.js
+++ scripts/table.js
@@ -1,17 +1,29 @@
 $(() => {
-    var uri = 'chart_item.json';
-    //JSON取得
+    
     $(document).ready(function () {
         // AJAXリクエスト送信
-        $.getJSON(uri)
+        $.getJSON('chart_item.json')
             .done(function (data) {
                 // 成功時処理
                 $.each(data, function (key, item) {
                     $('<tr>', { text: "" }).appendTo($('#mytable'));
                     $('<td>', { text: item.date }).appendTo($('#mytable'));
-                    $('<td>', { text: item.data }).appendTo($('#mytable'));
+                    $.each(item.data.split(','), function(index, value) {
+                        $('<td>', { text: value }).appendTo($('#mytable'));
+                    })
                 });
             });
+
+        $.getJSON('chart_config.json')
+            .done(function(data){
+                // 成功時処理
+                $('<tr>', { text: "" }).appendTo($('#mythead'));
+                $('<td>', { text: "Date" }).appendTo($('#mythead'));
+                $.each(data, function (key, item) {
+                    $('<td>', { text: item.name }).appendTo($('#mythead'));
+                })
+            });
     });
+
 
 });
